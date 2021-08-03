3fa6c2a024d003399168b733f5e65fb593fea065
Alex <jannyandalix@gmail.com>
2021-08-03 18:20:58 +0200

    Add new finctions ContainsInt

--- utils.go
+++ utils.go
@@ -2,8 +2,18 @@
 
 package utils
 
-// Contains provides search of work X in the slice A
+// Contains provides search the word X in the slice A
 func Contains(a []string, x string) bool {
+	for _, n := range a {
+		if x == n {
+			return true
+		}
+	}
+	return false
+}
+
+// ContainsInt provides search the digit X in the slice of ints A
+func ContainsInt(a []int, x int) bool {
 	for _, n := range a {
 		if x == n {
 			return true
